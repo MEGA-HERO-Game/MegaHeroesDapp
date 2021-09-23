6d0a18141303c38df16cfae1d53caa19807211aa
MEGA HERO Game <90598124+MHGameFi@users.noreply.github.com>
2021-09-23 16:46:14 +0800

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,7 +3,7 @@ let baseUrl;
 if(process.env.NODE_ENV === "development") { // 开发环境
   baseUrl = "/api";
 }else if(process.env.NODE_ENV === 'production') { // 线上环境
-  baseUrl = "http://mhweb.dev.xworld.pro:28080";
+  baseUrl = "http://mhweb.test.xworld.pro:28080";
 }
 
 export {
