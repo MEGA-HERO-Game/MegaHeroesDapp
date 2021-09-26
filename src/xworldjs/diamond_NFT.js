7e853934ad2c96e0619a25966beba178f55a9ecb
hexiangshuai <18311187480@163.com>
2021-09-27 01:03:24 +0800

    Update diamondNFT

--- src/xworldjs/diamond_NFT.js
+++ src/xworldjs/diamond_NFT.js
@@ -6,38 +6,50 @@ import Web3 from "web3";
 
 export class DiamondNFTContract {
   async init(provider, tokenAddress) {
-    const ccc = contract({"abi": DIAMOND_NFT_ABI});
+    const ccc = contract({ "abi": DIAMOND_NFT_ABI });
     ccc.setProvider(provider);
     this.contract = await ccc.at(tokenAddress);
   }
 
-  async buyDiamondNft(tokenid, amount, r, s, v, invitation, blockNumber, account, callback) {
-    const result = {}
-    await this.contract.buy(Web3.utils.toBN(tokenid), Web3.utils.toBN(amount), v, web3.utils.hexToBytes(r), web3.utils.hexToBytes(s), invitation, Web3.utils.toBN(blockNumber), {
-      from:
-        account
-    }).on('transactionHash', function (hash) {
-      console.info('hash:::', hash)
-      result.hash = hash
-    }).on('receipt', function () {
-      if (callback) {
-        callback({ hash: result.hash })
-      }
-      // console.info('receipt::::', receipt)
-    })
-      .on('confirmation', function () {
-        // console.info('confirmation:::', confirmationNumber, receipt)
-      })
-      .on('error', function (err) {
-        console.error('error:::', err)
-        if (callback) {
-          callback({
-            err: err,
-            code: err.code,
-            hash: result.hash
-          })
-        }
-      })
+  async buyDiamondNft(tokenid, amount, v, r, s, invitation, blockNumber, account, callback) {
+    return new Promise((resolve, reject) => {
+      let txHash = "";
+      this.contract.buy(tokenid, amount, v, r, s, invitation, blockNumber, {
+        from:
+          account
+      }).on('transactionHash', function (hash) {
+        txHash = hash;
+      }).on('receipt', function (receipt) {
+        resolve(txHash)
+      }).on('error', function (error) {
+        reject(error);
+      }).catch(error => {
+        reject(error);
+      });
+    });
+  }
 
+  async balanceOf(account, tokenid) {
+    // 获取用户钻石卡余额 （tokenid : 500,2000,5000）
+    return (await this.contract.balanceOf(account, tokenid)) / parseInt(getUsdtPointNumber())
+  }
+
+  async safeTransferFrom(account, diamondnftpoolAddress, tokenid, amount) {
+    // 质押钻石卡
+    return new Promise((resolve, reject) => {
+      let txHash = "";
+      this.contract.safeTransferFrom(account, diamondnftpoolAddress, tokenid, amount, "0x", {
+        from:
+          account
+      }).on('transactionHash', function (hash) {
+        txHash = hash;
+      }).on('receipt', function (receipt) {
+        resolve(txHash)
+      }).on('error', function (error) {
+        reject(error);
+      }).catch(error => {
+        reject(error);
+      });
+    });
   }
 }
