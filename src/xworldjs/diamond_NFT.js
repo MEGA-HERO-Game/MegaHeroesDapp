44d746544a3edbd79b66918a17d759acc5f46922
MEGA HERO Game <90598124+MHGameFi@users.noreply.github.com>
2021-11-01 11:22:40 +0800

    Update diamond_NFT.js

--- src/xworldjs/diamond_NFT.js
+++ src/xworldjs/diamond_NFT.js
@@ -10,26 +10,7 @@ export class DiamondNFTContract {
     ccc.setProvider(provider);
     this.contract = await ccc.at(tokenAddress);
   }
-  //充值购买游戏内钻石
-  async recharge(tokenid, amount, v, r, s, invitation, blockNumber, account, callback) {
-    return new Promise((resolve, reject) => {
-      let txHash = "";
-      this.contract.recharge(tokenid, amount, v, r, s, invitation, blockNumber, {
-        from:
-          account
-      }).on('transactionHash', function (hash) {
-        txHash = hash;
-      }).on('receipt', function (receipt) {
-        resolve(txHash)
-      }).on('error', function (error) {
-        reject(error);
-      }).catch(error => {
-        reject(error);
-      });
-    });
-  }
   
-
   async balanceOf(account, tokenid) {
     // 获取用户钻石卡余额 （tokenid : 500,2000,5000）
     return await this.contract.balanceOf(account, tokenid)
