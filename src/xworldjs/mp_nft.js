3df3ef6e0396f4ba269b04846594c99afbda17db
MEGA HERO Game <90598124+MHGameFi@users.noreply.github.com>
2021-11-08 11:29:04 +0800

    Update mp_nft.js

--- src/xworldjs/mp_nft.js
+++ src/xworldjs/mp_nft.js
@@ -13,27 +13,13 @@ export class MPNFTContract {
     const t = await this.contract.getUserNftList(user)
 
     console.log('nfts:::::', t)
-    const tmp = [];
     const result = [];
 
     t.map((v, i) => {
       console.log('v,i', v.toNumber(), i)
-      tmp.push({ id: v.toNumber() })
+      result.push({ id: v.toNumber() })
     })
-
-
-    for (const i of tmp) {
-      const amount = await this.contract.balanceOf(user, i.id)
-      // const uri = await this.contract.uri(i.id)
-      // console.log('amount:::', amount)
-      i.amount = await amount.toNumber()
-
-      if (i.amount > 0) {
-        result.push({ id: i.id, amount: i.amount })
-      }
-    }
     return result;
-    // console.log('result::::', result)
   }
   async safeTransferFrom(from, to, tokenid) {
     // 转账nft
